f0fbbfb7bbdf5f69c5113e52beb74d9a66d818ec
Rahul <rahulvaidya148@gmail.com>
2022-04-19 11:59:33 +0530

    Adding state in CartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 class CartItem extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img:''
+        }
+    }
+    
+    
+    
     render(){
+        const {price , title ,qty} = this.state
         return(
         <div className="cart-item">
             <div className="left-block">
@@ -10,9 +23,9 @@ class CartItem extends React.Component{
             </div>
 
             <div className="right-block">
-                <div style={{fontSize: 25}}>Phone</div>
-                <div style={{color:'#777'}}>Rs 999</div>
-                <div style={{color:'#777'}}>Qty. :1</div>
+                <div style={{fontSize: 25}}>{title}</div>
+                <div style={{color:'#777'}}>Rs: {price}</div>
+                <div style={{color:'#777'}}>Qty: {qty}</div>
             <div className="cart-item-actions">
             
             {/* Buttons */}
