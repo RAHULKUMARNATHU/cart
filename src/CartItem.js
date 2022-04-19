8d01b54a3d97d10b0b6c675a9abcecb2fb99b2ef
Rahul <rahulvaidya148@gmail.com>
2022-04-19 11:49:29 +0530

    CartItem::first component

--- src/CartItem.js
+++ src/CartItem.js
@@ -13,10 +13,13 @@ class CartItem extends React.Component{
                 <div style={{fontSize: 25}}>Phone</div>
                 <div style={{color:'#777'}}>Rs 999</div>
                 <div style={{color:'#777'}}>Qty. :1</div>
+            <div className="cart-item-actions">
+            
             {/* Buttons */}
-
-
-
+            <img alt="increase" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/2740/premium/2740679.png?token=exp=1650347918~hmac=18ef4059997bf855dd445626a09784ae"/>
+            <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png"/>
+            <img alt="delete" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/484/premium/484662.png?token=exp=1650348213~hmac=d1449d1193b4ae7de7d71ec1f09d8201"/>
+            </div>
             </div>
         </div>
         )}
