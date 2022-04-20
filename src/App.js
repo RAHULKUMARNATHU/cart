92f2291835b6d5b22dea7ea5ef572c0d4c68fe10
Rahul <rahulvaidya148@gmail.com>
2022-04-20 14:26:53 +0530

    Add total price

--- src/App.js
+++ src/App.js
@@ -1,13 +1,126 @@
+import React from "react";
 import Cart from "./Cart";
 import Navbar from "./Navbar";
 
-function App() {
-  return (
+class App extends React.Component {
+  
+  constructor() {
+    super();
+    this.state = {
+        products:[
+            {
+                price: 9999,
+                title: 'Mobile Phone',
+                qty: 1,
+                img:'https://images.unsplash.com/photo-1589492477829-5e65395b66cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+                id:1
+            },
+            {
+                price: 999,
+                title: 'Watch',
+                qty: 3,
+                img:'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
+                id:2
+            },
+            {
+                price: 979,
+                title: 'Football',
+                qty: 2,
+                img:'https://images.unsplash.com/photo-1511886929837-354d827aae26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
+                id:3
+            },
+            {
+                price: 59990,
+                title: 'Laptop',
+                qty: 5,
+                img:'https://images.unsplash.com/photo-1629131726692-1accd0c53ce0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bGFwdG9wc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+                id:4
+            }
+
+        ]
+    }
+    // this.increaseQuantity=this.increaseQuantity.bind(this)
+}
+handleIncreaseQuantity = (product) =>{
+    console.log("heyy plz increase the qty of",product);
+   
+    const {products} = this.state;
+     const index = products.indexOf(product);
+     products[index].qty += 1;
+
+     this.setState({
+         products : products
+     })
+}
+
+
+handleDecreaseQuantity = (product) =>{
+    console.log("heyy plz decrease the qty of",product);
+     const {products} = this.state;
+     const index = products.indexOf(product);
+
+     if(products[index].qty === 1){
+         return;
+     } 
+     products[index].qty -= 1;
+
+     this.setState({
+         products : products
+     })
+}
+
+handleDeleteProduct = (id) =>{
+    const {products} = this.state;
+
+    const items  = products.filter((item)=> item.id !== id);//gives the another array which element not matched id value
+    
+    this.setState({
+        products:items
+    })
+}
+
+getCartCount = () => {
+  const { products } = this.state;
+
+  let count = 0;
+
+  products.forEach((product) => {
+    count += product.qty;
+  })
+
+  return count;
+}
+
+getCartTotal = ()=>{
+  const {products} =this.state;
+
+  let cartTotal = 0;
+
+  products.map((product) =>{
+    cartTotal = cartTotal + product.qty * product.price 
+  })
+  return cartTotal;
+
+}
+  
+  render(){
+    const {products} = this.state
+   return (
     <div className="App">
-    <Navbar />
-    <Cart />
+    <Navbar count={this.getCartCount()} />
+    <Cart
+     products = {products}
+     onIncreaseQuantity= {this.handleIncreaseQuantity}
+     onDecreaseQuantity= {this.handleDecreaseQuantity}
+     onDeleteProduct = {this.handleDeleteProduct}
+      
+      />
+      <div style={{padding :10 , fontSize:20 }}>Total : {this.getCartTotal()}</div>
     </div>
   );
 }
+}
+
+
 
 export default App;
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,50 +1,47 @@
 import React from "react";
 
-class CartItem extends React.Component{
-     render(){
-        console.log("this.props",this.props)
-        const {price , title ,qty} = this.props.product;
-        const {
-            product ,
-             onIncreaseQuantity , 
-             onDecreaseQuantity,
-             onDeleteProduct
-            } = this.props;
-        return(
-        <div className="cart-item">
-            <div className="left-block">
-                <img style={styles.Image}/>
+const CartItem = (props) =>{
+    const {price , title ,qty} = props.product;
+    const {
+        product ,
+            onIncreaseQuantity , 
+            onDecreaseQuantity,
+            onDeleteProduct
+        } = props;
+    return(
+    <div className="cart-item">
+        <div className="left-block">
+            <img style={styles.Image} src={product.img} />
 
-            </div>
-
-            <div className="right-block">
-                <div style={{fontSize: 25}}>{title}</div>
-                <div style={{color:'#777'}}>Rs: {price}</div>
-                <div style={{color:'#777'}}>Qty: {qty}</div>
-            <div className="cart-item-actions">
-            
-            {/* Buttons */}
-            
-            <img alt="increase"
-             className="action-icons" 
-             src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
-             onClick= {() => onIncreaseQuantity(product)} /*to resolve the error::this.increaseQuantity.bind(this)*/
-             />
-             <img alt="deccrease" 
-            className="action-icons" 
-            src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
-            onClick={() =>onDecreaseQuantity(product)}
-            />
-            <img alt="delete" 
-            className="action-icons" 
-            src="https://cdn-icons.flaticon.com/png/512/484/premium/484662.png?token=exp=1650349931~hmac=89a26ae9371fd660378734e6e15e5792"
-            onClick={() => onDeleteProduct(product.id)}
-            />
-            </div>
-            </div>
         </div>
+
+        <div className="right-block">
+            <div style={{fontSize: 25}}>{title}</div>
+            <div style={{color:'#777'}}>Rs: {price}</div>
+            <div style={{color:'#777'}}>Qty: {qty}</div>
+        <div className="cart-item-actions">
+        
+        {/* Buttons */}
+        
+        <img alt="increase"
+            className="action-icons" 
+            src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+            onClick= {() => onIncreaseQuantity(product)} /*to resolve the error::this.increaseQuantity.bind(this)*/
+            />
+            <img alt="deccrease" 
+        className="action-icons" 
+        src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
+        onClick={() =>onDecreaseQuantity(product)}
+        />
+        <img alt="delete" 
+        className="action-icons" 
+        src="https://cdn-icons.flaticon.com/png/512/484/premium/484662.png?token=exp=1650349931~hmac=89a26ae9371fd660378734e6e15e5792"
+        onClick={() => onDeleteProduct(product.id)}
+        />
+        </div>
+        </div>
+    </div>
         )}
-}
 
 const styles = {
     Image:{
